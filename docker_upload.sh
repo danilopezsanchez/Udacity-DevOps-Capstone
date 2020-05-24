02ecb8be69ed730ad083734ded6b5dc073a4057d
Dani <danilopez.pc@gmail.com>
2020-05-24 16:20:39 +0200

    fix name

--- docker_upload.sh
+++ docker_upload.sh
@@ -2,12 +2,12 @@
 # This file tags and uploads an image to Docker Hub
 
 # Create dockerpath
-dockerpath=overrider/capstonetest
+dockerpath=overrider/capstone
 
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
 docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
-docker tag capstonetest $dockerpath
+docker tag capstone $dockerpath
 
 # Push image to a docker repository
 docker push $dockerpath
